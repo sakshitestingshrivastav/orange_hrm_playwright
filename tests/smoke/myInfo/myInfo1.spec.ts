4c69eff498e726dabf96e29988e2d7c40877872d
sakshitestingshrivastav <sakshitestingshrivastav@gmail.com>
2026-08-20 22:43:07 +0530

    updated myInfo test cases formatted

--- tests/smoke/myInfo/myInfo1.spec.ts
+++ tests/smoke/myInfo/myInfo1.spec.ts
@@ -15,42 +15,42 @@ test.describe('My Info page', () => {
   test('Verify that user is able to  enter the input value in last name', async ({ page }) => {
     await page.getByText('My Info').click();
     await expect(page.url()).toContain('/viewPersonalDetails/');
-    await page.getByPlaceholder("Last Name").fill('Test');
-    await expect(page.getByPlaceholder("Last Name")).toHaveValue('Test');
-    await page.getByPlaceholder("Last Name").clear()
-    await page.getByPlaceholder("Last Name").fill('Sakshi');
-    await expect(page.getByPlaceholder("Last Name")).toHaveValue('Sakshi');
+    await page.getByPlaceholder('Last Name').fill('Test');
+    await expect(page.getByPlaceholder('Last Name')).toHaveValue('Test');
+    await page.getByPlaceholder('Last Name').clear();
+    await page.getByPlaceholder('Last Name').fill('Sakshi');
+    await expect(page.getByPlaceholder('Last Name')).toHaveValue('Sakshi');
   });
 
   test('Verify that user is able to enter the input value in first name', async ({ page }) => {
     await page.getByText('My Info').click();
     await expect(page.url()).toContain('/viewPersonalDetails/');
-    await page.getByPlaceholder("First Name").fill('Test');
-    await expect(page.getByPlaceholder("First Name")).toHaveValue('Test');
-    await page.getByPlaceholder("First Name").clear()
+    await page.getByPlaceholder('First Name').fill('Test');
+    await expect(page.getByPlaceholder('First Name')).toHaveValue('Test');
+    await page.getByPlaceholder('First Name').clear();
     const str = generateString(20);
-    await page.getByPlaceholder("First Name").fill(str)
-    await expect(page.getByPlaceholder("First Name")).toHaveValue(str)
+    await page.getByPlaceholder('First Name').fill(str);
+    await expect(page.getByPlaceholder('First Name')).toHaveValue(str);
   });
 
   test('Verify that user is able to enter the input value in middle name', async ({ page }) => {
     await page.getByText('My Info').click();
     await expect(page.url()).toContain('/viewPersonalDetails/');
     const str = generateString(40);
-    await page.getByPlaceholder("Middle Name").fill(str)
-    await expect(page.getByPlaceholder("Middle Name")).toHaveValue(str)
+    await page.getByPlaceholder('Middle Name').fill(str);
+    await expect(page.getByPlaceholder('Middle Name')).toHaveValue(str);
     await page.getByRole('button', { name: 'Save' }).first().click();
-    await expect(page.getByText("Should not exceed 30 characters")).toBeVisible()
+    await expect(page.getByText('Should not exceed 30 characters')).toBeVisible();
   });
 
   test.only('Verify that user is able to enter driver value input', async ({ page }) => {
     await page.getByText('My Info').click();
     await expect(page.url()).toContain('/viewPersonalDetails/');
     const driverLicense = page
-    .locator('.oxd-input-group')
-    .filter({ hasText: "Driver's License Number" })
-    .locator('input');
-  await driverLicense.fill('123456');
-  await expect(driverLicense).toHaveValue('123456');
+      .locator('.oxd-input-group')
+      .filter({ hasText: "Driver's License Number" })
+      .locator('input');
+    await driverLicense.fill('123456');
+    await expect(driverLicense).toHaveValue('123456');
   });
-})
+});
