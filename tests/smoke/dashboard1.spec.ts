810bb1026e94fcb409aa508171c957aaaf402399
sakshitestingshrivastav <sakshitestingshrivastav@gmail.com>
2026-08-20 18:59:20 +0530

    updated the dashboard file more formatted

--- tests/smoke/dashboard1.spec.ts
+++ tests/smoke/dashboard1.spec.ts
@@ -11,36 +11,36 @@ test.describe('Dashboard page', () => {
     await expect(page).toHaveURL(process.env.DASHBOARD_URL!, { timeout: 30_000 });
   });
 
-  test("Verify that clicking on admin side bar link opens the admin page", async({page})=>{
+  test('Verify that clicking on admin side bar link opens the admin page', async ({ page }) => {
     const dashboardPage = new DashboardPage(page);
     await expect(page).toHaveURL(process.env.DASHBOARD_URL!, { timeout: 30_000 });
-    await page.getByText("Admin").click()
-    await expect(page.url()).toContain("/admin/")
-  })
+    await page.getByText('Admin').click();
+    await expect(page.url()).toContain('/admin/');
+  });
 
-  test("Verify that clicking on my info side bar link opens the my info page", async({page})=>{
+  test('Verify that clicking on my info side bar link opens the my info page', async ({ page }) => {
     const dashboardPage = new DashboardPage(page);
     await expect(page).toHaveURL(process.env.DASHBOARD_URL!, { timeout: 30_000 });
-    await page.getByText("My Info").click()
-    await expect(page.url()).toContain("/viewPersonalDetails/")
-  })
+    await page.getByText('My Info').click();
+    await expect(page.url()).toContain('/viewPersonalDetails/');
+  });
 
-  test.only("Verify", async({page})=>{
+  test.only('Verify', async ({ page }) => {
     const dashboardPage = new DashboardPage(page);
     await expect(page).toHaveURL(process.env.DASHBOARD_URL!, { timeout: 30_000 });
-    await page.getByText("My Info").click()
-    await expect(page.url()).toContain("/viewPersonalDetails/")
-    await page.locator('input[type="radio"]', {hasText : "Female"}).check()
-  })
+    await page.getByText('My Info').click();
+    await expect(page.url()).toContain('/viewPersonalDetails/');
+    await page.locator('input[type="radio"]', { hasText: 'Female' }).check();
+  });
 
-  test("Verify that clicking on upgrade link opens a new tab", async({page})=>{
+  test('Verify that clicking on upgrade link opens a new tab', async ({ page }) => {
     const dashboardPage = new DashboardPage(page);
     await expect(page).toHaveURL(process.env.DASHBOARD_URL!, { timeout: 30_000 });
     const [newPage] = await Promise.all([
-        page.waitForEvent('popup'),
-        page.getByRole('link', { name: /Upgrade/i }).click(),
-      ]);
-      await newPage.waitForLoadState();
-      await expect(newPage).toHaveURL(/.*\/upgrade-to-advanced.*/);
-    });
-  })
+      page.waitForEvent('popup'),
+      page.getByRole('link', { name: /Upgrade/i }).click(),
+    ]);
+    await newPage.waitForLoadState();
+    await expect(newPage).toHaveURL(/.*\/upgrade-to-advanced.*/);
+  });
+});
